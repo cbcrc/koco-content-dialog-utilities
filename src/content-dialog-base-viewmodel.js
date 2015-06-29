1ecbca01259956b6a71046745e84a2fd9e8e1ddd
Colin Prince <colin.prince@cbc.ca>
2015-06-29 16:15:47 -0400

    src files and bower.json

---
+++ src/content-dialog-base-viewmodel.js
@@ -0,0 +1,282 @@
+define([
+        'jquery',
+        'knockout',
+        'toastr',
+        'lodash',
+        'validation-utilities',
+        'mapping-utilities',
+        'disposer',
+        'i18next'
+    ],
+    function($, ko, toastr, _, koValidationUtilities, koMappingUtilities, KoDisposer, i18n) {
+        'use strict';
+
+        var ContentDialogViewModel = function(params) {
+            var self = this;
+
+            self.selectedInList = ko.observable();
+            self.mappingOptions = params.mappingOptions;
+            self.dialogTitle = params.dialogTitle;
+            self.originalItem = params.originalItem;
+            self.searchOnDisplay = self.getSearchOnDisplay();
+            self.defaultItem = params.defaultItem;
+            self.close = params.close;
+
+            //HACK: Il faudrait probablement créer un objet de base et un SearchableContentViewModel, à investiguer.
+            self.isSearchable = params.isSearchable;
+
+            //TODO: Investiguer pourquoi ca prend un "defaultItem" pour que les règles de validation s'appliquent
+            self.selectedItem = ko.validatedObservable(self.buildModel(params.defaultItem));
+
+            self.selectedItem.extend({
+                bootstrapValidation: {}
+            });
+
+            self.koDisposer = new KoDisposer();
+            self.koDisposer.add(self.selectedInList.subscribe(self.selectedInListChanged.bind(self)));
+
+            self.validatedObservables = [self.selectedItem];
+
+            self.originalItemSelected = ko.pureComputed(function() {
+                return self.getOriginalItemSelected();
+            });
+            self.koDisposer.add(self.originalItemSelected);
+
+            self.undoSelectionVisible = ko.pureComputed(function() {
+                return self.getUndoSelectionVisible();
+            });
+            self.koDisposer.add(self.undoSelectionVisible);
+
+            self.hasSelectedItem = ko.pureComputed(function() {
+                return self.getHasSelectedItem();
+            });
+            self.koDisposer.add(self.hasSelectedItem);
+        };
+
+        ContentDialogViewModel.prototype.getUndoSelectionVisible = function() {
+            var self = this;
+
+            return ko.unwrap(self.originalItem) && !self.originalItemSelected();
+        };
+
+        ContentDialogViewModel.prototype.getHasSelectedItem = function() {
+            var self = this;
+
+            var selectedItem = koMappingUtilities.toJS(self.selectedItem);
+
+            return !!(selectedItem && selectedItem.id);
+        };
+
+        ContentDialogViewModel.prototype.getOriginalItemSelected = function() {
+            var self = this;
+
+            var originalItem = koMappingUtilities.toJS(self.originalItem);
+
+            return !!(originalItem && self.isSame(originalItem));
+        };
+
+        ContentDialogViewModel.prototype.selectedInListChanged = function(selectedInfo) {
+            var self = this;
+
+            if (selectedInfo && selectedInfo.item) {
+                if (!self.isSame(selectedInfo.item)) {
+                    self.selectItem(selectedInfo.item);
+                }
+
+                if (selectedInfo.event.type === 'dblclick') {
+                    self.save();
+                }
+            }
+        };
+
+        ContentDialogViewModel.prototype.getSearchOnDisplay = function() {
+            var self = this;
+
+            return !ko.unwrap(self.originalItem);
+        };
+
+        ContentDialogViewModel.prototype.isSame = function(item) {
+            var self = this;
+
+            var selectedItem = koMappingUtilities.toJS(self.selectedItem);
+
+            return selectedItem && item && selectedItem.id === item.id;
+        };
+
+        ContentDialogViewModel.prototype.isOriginalItem = function(item) {
+            var self = this;
+
+            var originalItem = koMappingUtilities.toJS(self.originalItem);
+
+            return originalItem && item && originalItem.id === item.id;
+        };
+
+        ContentDialogViewModel.prototype.undoSelection = function() {
+            var self = this;
+
+            if (ko.unwrap(self.originalItem)) {
+                self.selectItem(ko.unwrap(self.originalItem));
+            }
+        };
+
+        ContentDialogViewModel.prototype.activate = function() {
+            var self = this;
+
+            if (self.loadLookups) {
+                return self.loadLookups().then(function() {
+                    return self.start();
+                });
+            } else {
+                return self.start();
+            }
+        };
+
+        ContentDialogViewModel.prototype.start = function() {
+            var self = this;
+
+            self.selectItem(ko.unwrap(self.originalItem));
+
+            return $.Deferred().resolve().promise();
+        };
+
+        ContentDialogViewModel.prototype.prepareScreenForValidationErrors = function(dfd) {
+            var self = this;
+
+            toastr.error(i18n.t('content-dialog-edit-form.form-error'));
+
+            $('html, body').animate({
+                scrollTop: 0
+            }, 'slow', function() {
+                dfd.resolve();
+            });
+        };
+
+        ContentDialogViewModel.prototype.selectItem = function(inputModel) {
+            var self = this;
+
+            var model = self.buildModel(inputModel);
+
+            self.selectedItem(model);
+        };
+
+        ContentDialogViewModel.prototype.buildModel = function(inputModel) {
+            var self = this;
+
+            var item = self.fromInputModel(inputModel);
+
+            if (item) {
+                self.addValidationToModel(item);
+            }
+
+            return item;
+        };
+
+        ContentDialogViewModel.prototype.fromInputModel = function(inputModel) {
+            var self = this;
+
+            if (inputModel) {
+                if (self.mappingOptions) {
+                    return ko.mapping.fromJS(inputModel, self.mappingOptions);
+                }
+
+                return ko.mapping.fromJS(inputModel);
+            }
+
+            return null;
+        };
+
+        ContentDialogViewModel.prototype.addValidationToModel = function( /*model*/ ) {
+            //var self = this;
+        };
+
+        ContentDialogViewModel.prototype.save = function() {
+            var self = this;
+
+            var promise = new $.Deferred(function(dfd) {
+                try {
+                    //self.serverSideValidationErrors([]);
+
+                    var validateError = self.validate();
+
+                    if (validateError) {
+                        toastr.error(validateError);
+
+                        $('html, body').animate({
+                            scrollTop: 0
+                        }, 'slow', function() {
+                            dfd.resolve();
+                        });
+                    } else {
+                        koValidationUtilities.validateObservables(self.validatedObservables)
+                            .then(function(isValid) {
+                                if (isValid) {
+                                    var writeModel = self.toOutputModel();
+                                    dfd.resolve();
+                                    self.close(writeModel);
+                                } else {
+                                    self.prepareScreenForValidationErrors(dfd);
+                                }
+                            }).fail(function() {
+                                dfd.reject.apply(this, arguments);
+                            });
+                    }
+                } catch (error) {
+                    dfd.reject(error);
+                }
+            }).promise();
+
+            promise.fail(function( /*error*/ ) {
+                //TODOX: logger error
+                //TODO: Appeler un fail global à l'app pour streamliner le tout
+                self.handleUnknownError();
+            });
+
+            return promise;
+        };
+
+        ContentDialogViewModel.prototype.validate = function(dfd) {
+            var self = this;
+
+            //HACK: Il faudrait probablement créer un objet de base et un SearchableContentViewModel, à investiguer.
+            if (self.isSearchable && self.hasSelectedItem() === false) {
+                return i18n.t('content-dialog-edit-form.no-item-selected');
+            }
+        };
+
+        ContentDialogViewModel.prototype.handleUnknownError = function( /*error*/ ) {
+            //todo: scoop api devrait retourner un 400 pour erreurs de validations mais il retourne un 500...
+            //toto: log erreur!?
+            //toastr.error(jqXhr.responseText);
+            toastr.error(i18n.t('content-dialog-edit-form.unknown-error'));
+        };
+
+        ContentDialogViewModel.prototype.toOutputModel = function() {
+            var self = this;
+
+            var outputModel = koMappingUtilities.toJS(self.selectedItem);
+
+            self.cleanOutputModel(outputModel);
+
+            return outputModel;
+        };
+
+        ContentDialogViewModel.prototype.cleanOutputModel = function( /*outputModel*/ ) {
+            //var self = this;
+
+            //CLeanup added properties
+        };
+
+        ContentDialogViewModel.prototype.cancel = function() {
+            var self = this;
+
+            self.close();
+        };
+
+        ContentDialogViewModel.prototype.dispose = function() {
+            var self = this;
+
+            self.koDisposer.dispose();
+        };
+
+        return ContentDialogViewModel;
+    });
---
+++ src/content-dialog-search-base-viewmodel.js
@@ -0,0 +1,287 @@
+//TODO: mixin avec content-list-page-viewmodel
+
+define([
+        'knockout',
+        'jquery',
+        'toastr',
+        'object-utilities',
+        'lodash',
+        'mapping-utilities',
+        'disposer'
+    ],
+    function(ko, $, toastr, objectUtilities, _, koMappingUtilities, KoDisposer) {
+        'use strict';
+
+        var defaultPagingFields = {
+            pageNumber: null,
+            pageSize: null
+        };
+
+        var ContentDialogSearchViewModelExtender = function(params) {
+            var self = this;
+
+            self.koDisposer = new KoDisposer();
+            self.selectedInList = ko.observable();
+            self.items = ko.observableArray();
+            self.apiResourceName = params.apiResourceName;
+            self.searchArguments = null;
+            self.searchOnDisplay = params.searchOnDisplay;
+            self.selected = params.selected;
+            self.hasSearched = ko.observable(false);
+            self.isSame = params.isSame;
+            self.isOriginalItem = params.isOriginalItem;
+            self.lastSearchSnapshot = params.lastSearchSnapshot;
+            self.searchArgumentsFields = Object.keys(params.defaultSearchFields).concat(Object.keys(defaultPagingFields));
+            self.searchFields = ko.mapping.fromJS(params.defaultSearchFields);
+            self.isSearchInProgress = ko.observable(self.searchOnDisplay);
+            self.searchQuery = null;
+            self.searchCanceled = false;
+            self.api = params.api;
+
+            //#region Paging
+            self.pagingFields = ko.mapping.fromJS(defaultPagingFields);
+            self.remainingItemsToLoad = ko.observable(false);
+            self.pagingDisabled = ko.pureComputed(function() {
+                return !self.remainingItemsToLoad() || self.isSearchInProgress();
+            });
+            self.koDisposer.add(self.pagingDisabled);
+
+            self.isPaging = ko.observable(false);
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.activate = function() {
+            var self = this;
+            var promisesStep1 = [];
+
+            var promise = $.when.apply($, promisesStep1).then(function() {
+                if (self.loadLookups) {
+                    return self.loadLookups().then(function() {
+                        return self.start();
+                    });
+                } else {
+                    return self.start();
+                }
+            });
+
+            return promise;
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.loadLookups = function() {
+            //var self = this;
+
+            return $.Deferred().resolve().promise();
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.start = function() {
+            var self = this;
+
+            self.correctLastSearchSnapshot(self.lastSearchSnapshot);
+            self.updateSearchFieldsFromLastSearchSnapshot();
+
+            if (self.searchOnDisplay) {
+                self.searchForItems();
+            }
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.cancelSearch = function() {
+            var self = this;
+
+            if (self.searchQuery) {
+                self.searchQuery.abort();
+            }
+
+            self.searchCanceled = true;
+            self.isSearchInProgress(false);
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.updateSearchArgumentsWithPagingFields = function() {
+            var self = this;
+
+            self.searchArguments = $.extend({}, self.searchArguments,
+                objectUtilities.pickNonFalsy(koMappingUtilities.toJS(self.pagingFields)));
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.search = function() {
+            var self = this;
+            self.isSearchInProgress(true);
+            self.searchCanceled = false;
+
+            self.searchQuery = self.api
+                .getJson(ko.unwrap(self.apiResourceName) , {
+                    data: $.param(self.searchArguments, false)
+                })
+                .done(function(pagedListOfItems) {
+                        self.hasSearched(true);
+                        self.bindSearchResults(pagedListOfItems);
+                })
+                .fail(function(jqXhr, textStatus, errorThrown) {
+                    //if (errorThrown) {
+                        if (errorThrown !== 'abort') {
+                            //toastr.error(errorThrown);
+                            toastr.error('Une erreur est survenue. Veuillez contacter Soutien MuSN: soutien.isn@radio-canada.ca');
+                        }
+                    //}
+                })
+                .always(function() {
+                    self.isSearchInProgress(false);
+
+                    if (!self.isPaging()) {
+                        if (self.$searchResultsElement && self.$searchResultsElement.length > 0) {
+                            self.$searchResultsElement.scrollTop(0);
+                        }
+                    }
+
+                    self.isPaging(false);
+                });
+
+            return self.searchQuery;
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.bindSearchResults = function(pagedListOfItems) {
+            var self = this;
+
+            if (!self.searchCanceled) {
+                pagedListOfItems.items = _.compact(pagedListOfItems.items);
+
+                self.searchCanceled = false;
+
+                var mappedData = self.addPropertiesToItems(pagedListOfItems.items);
+                var items = mappedData;
+
+                if (self.isPaging()) {
+                    items = self.items();
+
+                    for (var i = 0; i < mappedData.length; i++) {
+                        items.push(mappedData[i]);
+                    }
+                }
+
+                self.items(items);
+                self.remainingItemsToLoad(self.items().length < pagedListOfItems.totalNumberOfItems);
+                self.updateViewModelPagingFields(pagedListOfItems);
+            }
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.goToNextPage = function() {
+            var self = this;
+
+            self.pagingFields.pageNumber((self.pagingFields.pageNumber() || 1) + 1);
+            self.isPaging(true);
+            self.updateSearchArgumentsWithPagingFields();
+
+            return self.search();
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.searchForItems = function() {
+            var self = this;
+
+            var searchFields = koMappingUtilities.toJS(self.searchFields);
+            self.lastSearchSnapshot.searchFields = searchFields;
+            self.pagingFields.pageNumber(null);
+            self.itemsDispose();
+            self.hasSearched(false);
+
+            self.searchArguments = self.getSearchArgumentsFromFields();
+            self.updateSearchArgumentsWithPagingFields();
+
+            return self.search();
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.getSearchArgumentsFromFields = function() {
+            var searchFields = koMappingUtilities.toJS(this.searchFields);
+            var searchArguments = _.pick(searchFields, this.searchArgumentsFields);
+
+            return searchArguments;
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.updateSearchFieldsFromLastSearchSnapshot = function() {
+            var self = this;
+
+            if (self.lastSearchSnapshot.searchFields) {
+                ko.mapping.fromJS(self.lastSearchSnapshot.searchFields, {}, self.searchFields);
+            }
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.addPropertiesToItem = function(item) {
+            var self = this;
+
+            if (item) {
+                item.isSelected = ko.pureComputed(function() {
+                    return self.isSame(item);
+                });
+            }
+
+            return item;
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.addPropertiesToItems = function(data) {
+            var self = this;
+
+            var mappedData = ko.utils.arrayMap(data, function(item) {
+                return self.addPropertiesToItem(item);
+            });
+
+            return mappedData;
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.selectItem = function(item, event) {
+            var self = this;
+
+            self.selected({
+                item: self.cleanUpItem(item),
+                event: event
+            });
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.cleanUpItem = function(item) {
+            //var self = this;
+            var result = _.cloneDeep(item);
+
+            //remove added properties
+            delete result.isSelected;
+
+            return result;
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.dispose = function() {
+            var self = this;
+
+            self.pagingDisabled.dispose();
+            self.itemsDispose();
+            self.koDisposer.dispose();
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.itemsDispose = function() {
+            var self = this;
+
+            var items = self.items();
+
+            _(items).forEach(function(item) {
+                self.itemDispose(item);
+            });
+
+            self.items([]);
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.itemDispose = function(item) {
+            //var self = this;
+
+            item.isSelected.dispose();
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.updateViewModelPagingFields = function(pagedListOfIndexedDocuments) {
+            var self = this;
+
+            self.pagingFields.pageNumber(pagedListOfIndexedDocuments.pageNumber);
+            self.pagingFields.pageSize(pagedListOfIndexedDocuments.pageSize);
+        };
+
+        ContentDialogSearchViewModelExtender.prototype.correctLastSearchSnapshot = function(lastSearchSnapshot) {
+            //var self = this;
+
+
+            return lastSearchSnapshot;
+        };
+
+        return ContentDialogSearchViewModelExtender;
+    });
