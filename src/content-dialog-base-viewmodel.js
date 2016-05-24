15f87da8bfb037183a39a42786a56177494248dd
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:20:02 -0400

    update to koco2

--- src/content-dialog-base-viewmodel.js
+++ src/content-dialog-base-viewmodel.js
@@ -1,282 +1,278 @@
-define([
-        'jquery',
-        'knockout',
-        'toastr',
-        'lodash',
-        'validation-utilities',
-        'mapping-utilities',
-        'disposer',
-        'i18next'
-    ],
-    function($, ko, toastr, _, koValidationUtilities, koMappingUtilities, KoDisposer, i18n) {
-        'use strict';
+import $ from 'jquery';
+import ko from 'knockout';
+import toastr from 'toastr';
+import _ from 'lodash';
+import koValidationUtilities from 'validation-utilities';
+import koMappingUtilities from 'mapping-utilities';
+import KoDisposer from 'disposer';
+import i18n from 'i18next';
 
-        var ContentDialogViewModel = function(params) {
-            var self = this;
 
-            self.selectedInList = ko.observable();
-            self.mappingOptions = params.mappingOptions;
-            self.dialogTitle = params.dialogTitle;
-            self.originalItem = params.originalItem;
-            self.searchOnDisplay = self.getSearchOnDisplay();
-            self.defaultItem = params.defaultItem;
-            self.close = params.close;
+var ContentDialogViewModel = function(params) {
+    var self = this;
 
-            //HACK: Il faudrait probablement créer un objet de base et un SearchableContentViewModel, à investiguer.
-            self.isSearchable = params.isSearchable;
+    self.selectedInList = ko.observable();
+    self.mappingOptions = params.mappingOptions;
+    self.dialogTitle = params.dialogTitle;
+    self.originalItem = params.originalItem;
+    self.searchOnDisplay = self.getSearchOnDisplay();
+    self.defaultItem = params.defaultItem;
+    self.close = params.close;
 
-            //TODO: Investiguer pourquoi ca prend un "defaultItem" pour que les règles de validation s'appliquent
-            self.selectedItem = ko.validatedObservable(self.buildModel(params.defaultItem));
+    //HACK: Il faudrait probablement créer un objet de base et un SearchableContentViewModel, à investiguer.
+    self.isSearchable = params.isSearchable;
 
-            self.selectedItem.extend({
-                bootstrapValidation: {}
-            });
+    //TODO: Investiguer pourquoi ca prend un "defaultItem" pour que les règles de validation s'appliquent
+    self.selectedItem = ko.validatedObservable(self.buildModel(params.defaultItem));
 
-            self.koDisposer = new KoDisposer();
-            self.koDisposer.add(self.selectedInList.subscribe(self.selectedInListChanged.bind(self)));
+    self.selectedItem.extend({
+        bootstrapValidation: {}
+    });
 
-            self.validatedObservables = [self.selectedItem];
+    self.koDisposer = new KoDisposer();
+    self.koDisposer.add(self.selectedInList.subscribe(self.selectedInListChanged.bind(self)));
 
-            self.originalItemSelected = ko.pureComputed(function() {
-                return self.getOriginalItemSelected();
-            });
-            self.koDisposer.add(self.originalItemSelected);
+    self.validatedObservables = [self.selectedItem];
 
-            self.undoSelectionVisible = ko.pureComputed(function() {
-                return self.getUndoSelectionVisible();
-            });
-            self.koDisposer.add(self.undoSelectionVisible);
+    self.originalItemSelected = ko.pureComputed(function() {
+        return self.getOriginalItemSelected();
+    });
+    self.koDisposer.add(self.originalItemSelected);
 
-            self.hasSelectedItem = ko.pureComputed(function() {
-                return self.getHasSelectedItem();
-            });
-            self.koDisposer.add(self.hasSelectedItem);
-        };
+    self.undoSelectionVisible = ko.pureComputed(function() {
+        return self.getUndoSelectionVisible();
+    });
+    self.koDisposer.add(self.undoSelectionVisible);
 
-        ContentDialogViewModel.prototype.getUndoSelectionVisible = function() {
-            var self = this;
+    self.hasSelectedItem = ko.pureComputed(function() {
+        return self.getHasSelectedItem();
+    });
+    self.koDisposer.add(self.hasSelectedItem);
+};
 
-            return ko.unwrap(self.originalItem) && !self.originalItemSelected();
-        };
+ContentDialogViewModel.prototype.getUndoSelectionVisible = function() {
+    var self = this;
 
-        ContentDialogViewModel.prototype.getHasSelectedItem = function() {
-            var self = this;
+    return ko.unwrap(self.originalItem) && !self.originalItemSelected();
+};
 
-            var selectedItem = koMappingUtilities.toJS(self.selectedItem);
+ContentDialogViewModel.prototype.getHasSelectedItem = function() {
+    var self = this;
 
-            return !!(selectedItem && selectedItem.id);
-        };
+    var selectedItem = koMappingUtilities.toJS(self.selectedItem);
 
-        ContentDialogViewModel.prototype.getOriginalItemSelected = function() {
-            var self = this;
+    return !!(selectedItem && selectedItem.id);
+};
 
-            var originalItem = koMappingUtilities.toJS(self.originalItem);
+ContentDialogViewModel.prototype.getOriginalItemSelected = function() {
+    var self = this;
 
-            return !!(originalItem && self.isSame(originalItem));
-        };
+    var originalItem = koMappingUtilities.toJS(self.originalItem);
 
-        ContentDialogViewModel.prototype.selectedInListChanged = function(selectedInfo) {
-            var self = this;
+    return !!(originalItem && self.isSame(originalItem));
+};
 
-            if (selectedInfo && selectedInfo.item) {
-                if (!self.isSame(selectedInfo.item)) {
-                    self.selectItem(selectedInfo.item);
-                }
+ContentDialogViewModel.prototype.selectedInListChanged = function(selectedInfo) {
+    var self = this;
 
-                if (selectedInfo.event.type === 'dblclick') {
-                    self.save();
-                }
-            }
-        };
+    if (selectedInfo && selectedInfo.item) {
+        if (!self.isSame(selectedInfo.item)) {
+            self.selectItem(selectedInfo.item);
+        }
 
-        ContentDialogViewModel.prototype.getSearchOnDisplay = function() {
-            var self = this;
+        if (selectedInfo.event.type === 'dblclick') {
+            self.save();
+        }
+    }
+};
 
-            return !ko.unwrap(self.originalItem);
-        };
+ContentDialogViewModel.prototype.getSearchOnDisplay = function() {
+    var self = this;
 
-        ContentDialogViewModel.prototype.isSame = function(item) {
-            var self = this;
+    return !ko.unwrap(self.originalItem);
+};
 
-            var selectedItem = koMappingUtilities.toJS(self.selectedItem);
+ContentDialogViewModel.prototype.isSame = function(item) {
+    var self = this;
 
-            return selectedItem && item && selectedItem.id === item.id;
-        };
+    var selectedItem = koMappingUtilities.toJS(self.selectedItem);
 
-        ContentDialogViewModel.prototype.isOriginalItem = function(item) {
-            var self = this;
+    return selectedItem && item && selectedItem.id === item.id;
+};
 
-            var originalItem = koMappingUtilities.toJS(self.originalItem);
+ContentDialogViewModel.prototype.isOriginalItem = function(item) {
+    var self = this;
 
-            return originalItem && item && originalItem.id === item.id;
-        };
+    var originalItem = koMappingUtilities.toJS(self.originalItem);
 
-        ContentDialogViewModel.prototype.undoSelection = function() {
-            var self = this;
+    return originalItem && item && originalItem.id === item.id;
+};
 
-            if (ko.unwrap(self.originalItem)) {
-                self.selectItem(ko.unwrap(self.originalItem));
-            }
-        };
+ContentDialogViewModel.prototype.undoSelection = function() {
+    var self = this;
 
-        ContentDialogViewModel.prototype.activate = function() {
-            var self = this;
+    if (ko.unwrap(self.originalItem)) {
+        self.selectItem(ko.unwrap(self.originalItem));
+    }
+};
 
-            if (self.loadLookups) {
-                return self.loadLookups().then(function() {
-                    return self.start();
+ContentDialogViewModel.prototype.activate = function() {
+    var self = this;
+
+    if (self.loadLookups) {
+        return self.loadLookups().then(function() {
+            return self.start();
+        });
+    } else {
+        return self.start();
+    }
+};
+
+ContentDialogViewModel.prototype.start = function() {
+    var self = this;
+
+    self.selectItem(ko.unwrap(self.originalItem));
+
+    return $.Deferred().resolve().promise();
+};
+
+ContentDialogViewModel.prototype.prepareScreenForValidationErrors = function(dfd) {
+    var self = this;
+
+    toastr.error(i18n.t('koco-content-dialog-utilities.content-dialog-edit-form.form-error'));
+
+    $('html, body').animate({
+        scrollTop: 0
+    }, 'slow', function() {
+        dfd.resolve();
+    });
+};
+
+ContentDialogViewModel.prototype.selectItem = function(inputModel) {
+    var self = this;
+
+    var model = self.buildModel(inputModel);
+
+    self.selectedItem(model);
+};
+
+ContentDialogViewModel.prototype.buildModel = function(inputModel) {
+    var self = this;
+
+    var item = self.fromInputModel(inputModel);
+
+    if (item) {
+        self.addValidationToModel(item);
+    }
+
+    return item;
+};
+
+ContentDialogViewModel.prototype.fromInputModel = function(inputModel) {
+    var self = this;
+
+    if (inputModel) {
+        if (self.mappingOptions) {
+            return ko.mapping.fromJS(inputModel, self.mappingOptions);
+        }
+
+        return ko.mapping.fromJS(inputModel);
+    }
+
+    return null;
+};
+
+ContentDialogViewModel.prototype.addValidationToModel = function( /*model*/ ) {
+    //var self = this;
+};
+
+ContentDialogViewModel.prototype.save = function() {
+    var self = this;
+
+    var promise = new $.Deferred(function(dfd) {
+        try {
+            //self.serverSideValidationErrors([]);
+
+            var validateError = self.validate();
+
+            if (validateError) {
+                toastr.error(validateError);
+
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 'slow', function() {
+                    dfd.resolve();
                 });
             } else {
-                return self.start();
-            }
-        };
-
-        ContentDialogViewModel.prototype.start = function() {
-            var self = this;
-
-            self.selectItem(ko.unwrap(self.originalItem));
-
-            return $.Deferred().resolve().promise();
-        };
-
-        ContentDialogViewModel.prototype.prepareScreenForValidationErrors = function(dfd) {
-            var self = this;
-
-            toastr.error(i18n.t('koco-content-dialog-utilities.content-dialog-edit-form.form-error'));
-
-            $('html, body').animate({
-                scrollTop: 0
-            }, 'slow', function() {
-                dfd.resolve();
-            });
-        };
-
-        ContentDialogViewModel.prototype.selectItem = function(inputModel) {
-            var self = this;
-
-            var model = self.buildModel(inputModel);
-
-            self.selectedItem(model);
-        };
-
-        ContentDialogViewModel.prototype.buildModel = function(inputModel) {
-            var self = this;
-
-            var item = self.fromInputModel(inputModel);
-
-            if (item) {
-                self.addValidationToModel(item);
-            }
-
-            return item;
-        };
-
-        ContentDialogViewModel.prototype.fromInputModel = function(inputModel) {
-            var self = this;
-
-            if (inputModel) {
-                if (self.mappingOptions) {
-                    return ko.mapping.fromJS(inputModel, self.mappingOptions);
-                }
-
-                return ko.mapping.fromJS(inputModel);
-            }
-
-            return null;
-        };
-
-        ContentDialogViewModel.prototype.addValidationToModel = function( /*model*/ ) {
-            //var self = this;
-        };
-
-        ContentDialogViewModel.prototype.save = function() {
-            var self = this;
-
-            var promise = new $.Deferred(function(dfd) {
-                try {
-                    //self.serverSideValidationErrors([]);
-
-                    var validateError = self.validate();
-
-                    if (validateError) {
-                        toastr.error(validateError);
-
-                        $('html, body').animate({
-                            scrollTop: 0
-                        }, 'slow', function() {
+                koValidationUtilities.validateObservables(self.validatedObservables)
+                    .then(function(isValid) {
+                        if (isValid) {
+                            var writeModel = self.toOutputModel();
                             dfd.resolve();
-                        });
-                    } else {
-                        koValidationUtilities.validateObservables(self.validatedObservables)
-                            .then(function(isValid) {
-                                if (isValid) {
-                                    var writeModel = self.toOutputModel();
-                                    dfd.resolve();
-                                    self.close(writeModel);
-                                } else {
-                                    self.prepareScreenForValidationErrors(dfd);
-                                }
-                            }).fail(function() {
-                                dfd.reject.apply(this, arguments);
-                            });
-                    }
-                } catch (error) {
-                    dfd.reject(error);
-                }
-            }).promise();
-
-            promise.fail(function( /*error*/ ) {
-                //TODOX: logger error
-                //TODO: Appeler un fail global à l'app pour streamliner le tout
-                self.handleUnknownError();
-            });
-
-            return promise;
-        };
-
-        ContentDialogViewModel.prototype.validate = function(dfd) {
-            var self = this;
-
-            //HACK: Il faudrait probablement créer un objet de base et un SearchableContentViewModel, à investiguer.
-            if (self.isSearchable && self.hasSelectedItem() === false) {
-                return i18n.t('koco-content-dialog-utilities.content-dialog-edit-form.no-item-selected');
+                            self.close(writeModel);
+                        } else {
+                            self.prepareScreenForValidationErrors(dfd);
+                        }
+                    }).fail(function() {
+                        dfd.reject.apply(this, arguments);
+                    });
             }
-        };
+        } catch (error) {
+            dfd.reject(error);
+        }
+    }).promise();
 
-        ContentDialogViewModel.prototype.handleUnknownError = function( /*error*/ ) {
-            //todo: scoop api devrait retourner un 400 pour erreurs de validations mais il retourne un 500...
-            //toto: log erreur!?
-            //toastr.error(jqXhr.responseText);
-            toastr.error(i18n.t('koco-content-dialog-utilities.content-dialog-edit-form.unknown-error'));
-        };
-
-        ContentDialogViewModel.prototype.toOutputModel = function() {
-            var self = this;
-
-            var outputModel = koMappingUtilities.toJS(self.selectedItem);
-
-            self.cleanOutputModel(outputModel);
-
-            return outputModel;
-        };
-
-        ContentDialogViewModel.prototype.cleanOutputModel = function( /*outputModel*/ ) {
-            //var self = this;
-
-            //CLeanup added properties
-        };
-
-        ContentDialogViewModel.prototype.cancel = function() {
-            var self = this;
-
-            self.close();
-        };
-
-        ContentDialogViewModel.prototype.dispose = function() {
-            var self = this;
-
-            self.koDisposer.dispose();
-        };
-
-        return ContentDialogViewModel;
+    promise.fail(function( /*error*/ ) {
+        //TODOX: logger error
+        //TODO: Appeler un fail global à l'app pour streamliner le tout
+        self.handleUnknownError();
     });
+
+    return promise;
+};
+
+ContentDialogViewModel.prototype.validate = function(dfd) {
+    var self = this;
+
+    //HACK: Il faudrait probablement créer un objet de base et un SearchableContentViewModel, à investiguer.
+    if (self.isSearchable && self.hasSelectedItem() === false) {
+        return i18n.t('koco-content-dialog-utilities.content-dialog-edit-form.no-item-selected');
+    }
+};
+
+ContentDialogViewModel.prototype.handleUnknownError = function( /*error*/ ) {
+    //todo: scoop api devrait retourner un 400 pour erreurs de validations mais il retourne un 500...
+    //toto: log erreur!?
+    //toastr.error(jqXhr.responseText);
+    toastr.error(i18n.t('koco-content-dialog-utilities.content-dialog-edit-form.unknown-error'));
+};
+
+ContentDialogViewModel.prototype.toOutputModel = function() {
+    var self = this;
+
+    var outputModel = koMappingUtilities.toJS(self.selectedItem);
+
+    self.cleanOutputModel(outputModel);
+
+    return outputModel;
+};
+
+ContentDialogViewModel.prototype.cleanOutputModel = function( /*outputModel*/ ) {
+    //var self = this;
+
+    //CLeanup added properties
+};
+
+ContentDialogViewModel.prototype.cancel = function() {
+    var self = this;
+
+    self.close();
+};
+
+ContentDialogViewModel.prototype.dispose = function() {
+    var self = this;
+
+    self.koDisposer.dispose();
+};
+
+export default ContentDialogViewModel;
